2f73aa1f8e10db5644a28176ead06ad85fc7ee7f
Kazuya <iam@kazuyahara.dev>
2022-11-07 23:06:17 +0900

    feat(web): list with section title

--- web/src/adapters/userInterface/components/organisms/sectionList/item/index.tsx
+++ web/src/adapters/userInterface/components/organisms/sectionList/item/index.tsx
@@ -1,20 +1,39 @@
 import React from 'react';
 
-import { Grid, GridProps } from '@mui/material';
+import { Box, BoxProps, Grid, Typography } from '@mui/material';
+import { format } from 'date-fns';
 
 import { ItemWithURL } from '../../../../../../domains/item';
 import AspectRetioImage from '../../../atoms/aspectRatioImage';
 
-type Props = Pick<GridProps, 'sx'> & { items: ItemWithURL[] };
+type Props = Pick<BoxProps, 'sx'> & { items: ItemWithURL[] };
 
 export default function ItemSectionList({ items, sx }: Props) {
+  const months = items
+    .map((item) => format(item.date, 'yyyy-MM'))
+    .filter((elem, index, self) => self.indexOf(elem) === index);
+  const sections = months.map((month) => {
+    const list = items.filter((item) => format(item.date, 'yyyy-MM') === month);
+    const title = format(list[0].date, 'yyyy年M月');
+    return { list, title };
+  });
+
   return (
-    <Grid container spacing={1} sx={sx}>
-      {items.map((item) => (
-        <Grid item key={item.id} xs={4} sm={3} md={2}>
-          <AspectRetioImage borderRadius={1} src={item.url} />
-        </Grid>
+    <Box sx={sx}>
+      {sections.map((section) => (
+        <Box key={section.title} mb={3}>
+          <Typography gutterBottom variant="h2">
+            {section.title}
+          </Typography>
+          <Grid container spacing={1}>
+            {section.list.map((item) => (
+              <Grid item key={item.id} xs={4} sm={3} md={2}>
+                <AspectRetioImage borderRadius={1} src={item.url} />
+              </Grid>
+            ))}
+          </Grid>
+        </Box>
       ))}
-    </Grid>
+    </Box>
   );
 }
